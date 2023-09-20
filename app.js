c528155e2414d1952e7d20fb2b461333b3c88700
SoutArturo09 <as5237189@gmail.com>
2023-09-20 00:01:01 -0600

    nodemon agregado

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
- 
+ const path = require("path")
 
 const app = express();
 
 app.get("/", (req,res)=>{
-    //res.send("hello port 8009");
-    res.sendFile("../server-node/index.html")
+        //res.send("hello port 8009");
+
+    res.sendFile(path.join(__dirname + "/index.html"))
 })
 
 app.listen(8009,()=>{
-    console.log("server running on port", 8009);
+    console.log("server listening on port", 8009);
 })
